13c6f4928ac071827bf3ae4dc5a8a982c7c8d06a
zorro901 <zoro907@gmail.com>
2022-10-17 07:09:42 +0900

    Add sample data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Layout from "../components/Layout";
 import { siteConfig } from "../site.config";
+import { sampleCards } from "../utils/sample";
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +10,9 @@ const Home: NextPage = () => {
         <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
         <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
           {/* Card */}
+          {sampleCards.map((page, index) => (
+            <p key={index}>{page.name}</p>
+          ))}
         </div>
       </div>
     </Layout>

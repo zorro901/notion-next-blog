fc6fed502a86858acf017a5a9b5512580dd5c557
zorro901 <zoro907@gmail.com>
2022-10-17 07:06:19 +0900

    Modify layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,16 @@
+import { FC } from "react";
+import { LayoutProps } from "../types/types";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-const Layout = () => {
+const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="relative overflow-hidden">
       <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
         {/*  */}
         <Navbar />
         {/*  */}
-        <main></main>
+        <main className="w-full pb-12 px-4">{children}</main>
         {/*  */}
         <Footer />
       </div>

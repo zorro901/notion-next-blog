b81bd1dd95efe7657c3ae2790725797778101705
zorro901 <zoro907@gmail.com>
2022-10-17 06:57:28 +0900

    Add Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
-  return (
-    <>
+  return <Layout />;
+};
 
-    </>
-  )
-}
-
-export default Home
+export default Home;

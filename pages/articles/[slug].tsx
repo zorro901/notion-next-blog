7d7c98f696b042b2a6042ece76ef5dd518aa30b2
zorro901 <zoro907@gmail.com>
2022-10-17 07:29:28 +0900

    Add ArticleMeta

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import React from "react";
+import ArticleMeta from "../../components/ArticleMeta";
 import Layout from "../../components/Layout";
 
 const Article = () => {
@@ -8,37 +7,7 @@ const Article = () => {
       <article className="w-full">
         {/* meta section */}
         <div className="my-12">
-          {/* page cover */}
-          <Image
-            className="w-full max-w-screen-lg rounded-lg aspect-video my-4"
-            src={"/"}
-            alt=""
-            objectFit="cover"
-            width={640}
-            height={360}
-            quality={50}
-          />
-
-          {/* page name */}
-          <h1 className="my-8">page name</h1>
-          <div className="bg-gray-100 px-6 py-4 rounded text-sm text-gray-500">
-            <div className="grid grid-cols-3 gap-4">
-              {/* published */}
-              <div className="col-span-1">Published</div>
-              <div className="col-span-2">published</div>
-
-              {/* author */}
-              <div className="col-span-1">Author</div>
-              <div className="col-span-2">author</div>
-
-              {/* tags */}
-              <div className="col-span-1">Tags</div>
-              <div className="col-span-2">
-                {/* change later */}
-                <span>#tag</span>
-              </div>
-            </div>
-          </div>
+          <ArticleMeta />
         </div>
 
         {/* article */}
@@ -49,4 +18,3 @@ const Article = () => {
 };
 
 export default Article;
-

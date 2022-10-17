b1b3911ecc6d2c3e75e8724f0a463187e02f4ca9
zorro901 <zoro907@gmail.com>
2022-10-17 09:09:45 +0900

    Modify card component to use generic functions

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { GetStaticProps, NextPage } from "next";
 import Card from "../components/Card";
 import Layout from "../components/Layout";
 import { siteConfig } from "../site.config";
-import { sampleCards } from "../utils/sample";
 import { fetchPages } from "../utils/notion";
+import { IndexProps } from "../types/types";
 
 export const getStaticProps: GetStaticProps = async () => {
   const { results } = await fetchPages()
@@ -15,14 +15,14 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
-const Home: NextPage = ({pages}) => {
+const Home: NextPage<IndexProps> = ({pages}) => {
   return (
     <Layout>
       <div className="pt-12">
         <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
         <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
           {/* Card */}
-          {sampleCards.map((page, index) => (
+          {pages.map((page, index) => (
             <Card key={index} page={page} />
           ))}
         </div>

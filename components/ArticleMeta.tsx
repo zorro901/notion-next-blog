a7c5c36a2d7e8e8265af8a8334e3bae950e91567
zorro901 <zoro907@gmail.com>
2022-10-18 08:35:12 +0900

    Modify articleMeta component to use generic functions

--- components/ArticleMeta.tsx
+++ components/ArticleMeta.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { FC } from "react";
 import { ArticleMetaProps } from "../types/types";
+import { getCover, getDate, getMultiSelect, getText } from '../utils/property';
 
 const ArticleMeta: FC<ArticleMetaProps> = ({ page }) => {
   return (
@@ -8,7 +9,7 @@ const ArticleMeta: FC<ArticleMetaProps> = ({ page }) => {
       {/* page cover */}
       <Image
         className="w-full max-w-screen-lg rounded-lg aspect-video my-4"
-        src={page.cover}
+        src={getCover(page.cover)}
         alt=""
         objectFit="cover"
         width={640}
@@ -17,22 +18,22 @@ const ArticleMeta: FC<ArticleMetaProps> = ({ page }) => {
       />
 
       {/* page name */}
-      <h1 className="my-8">{page.name}</h1>
+      <h1 className="my-8">{getText(page.properties.name.title)}</h1>
       <div className="bg-gray-100 px-6 py-4 rounded text-sm text-gray-500">
         <div className="grid grid-cols-3 gap-4">
           {/* published */}
           <div className="col-span-1">Published</div>
-          <div className="col-span-2">{page.published}</div>
+          <div className="col-span-2">{getDate(page.properties.published.date)}</div>
 
           {/* author */}
           <div className="col-span-1">Author</div>
-          <div className="col-span-2">{page.author}</div>
+          <div className="col-span-2">{getText(page.properties.author.rich_text)}</div>
 
           {/* tags */}
           <div className="col-span-1">Tags</div>
           <div className="col-span-2">
             {/* change later */}
-            {page.tags.map((tag: string, index: number) => (
+            {getMultiSelect(page.properties.tags.multi_select).map((tag: string, index: number) => (
               <span key={index}>{`#${tag} `}</span>
             ))}
           </div>

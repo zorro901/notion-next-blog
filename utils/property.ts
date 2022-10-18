b9f0b3bc0eab94a0540716c12dedf9c08b1231d0
zorro901 <zoro907@gmail.com>
2022-10-18 22:13:34 +0900

    Add no_image

--- utils/property.ts
+++ utils/property.ts
@@ -13,7 +13,7 @@ export const getText = (richTextArr: RichTextType[]) => {
 export const getCover = (cover: PageType['cover']): string => {
   if (cover && cover.file) return cover.file.url
   if (cover && cover.external) return cover.external.url
-  return '/noimage.ong'
+  return '/no_image.png'
 }
 
 export const getDate = (date: { start: string }): string => {
